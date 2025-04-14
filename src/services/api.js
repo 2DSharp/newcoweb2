de6ec98021287e656ea4e6a25209bae636cbe950
2dsharp <30053071+2DSharp@users.noreply.github.com>
2025-04-15 02:54:13 +0530

    Don't track .env

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ import {AuthService} from "@/services/authService";
 
 // API Configuration
 const API_CONFIG = {
-    baseURL: 'http://localhost:8080',
+    baseURL: process.env.BASE_API_BASEURL,
     timeout: parseInt(process.env.VITE_API_TIMEOUT || '60000'),
     headers: {
         'Content-Type': 'application/json',
@@ -120,7 +120,7 @@ const getContextualApiClient = (isSellerContext = false) => {
 const authenticatedApiClient = axios.create(API_CONFIG);
 
 export const authenticatedFileUploadClient = axios.create({
-    baseURL: "http://localhost:5000"
+    baseURL: process.env.ADV_API_BASEURL
 });
 
 export const unauthenticatedApiClient = axios.create({
@@ -128,7 +128,7 @@ export const unauthenticatedApiClient = axios.create({
 });
 
 export const unauthenticatedSearchClient = axios.create({
-    baseURL: "http://localhost:5000"
+    baseURL: process.env.ADV_API_BASEURL
 });
 
 // Request Interceptor
